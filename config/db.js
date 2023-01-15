a6deb958cefb54eea68c01177038df981e4e4913
CB-Pantomime <christopherbrown.contact@gmail.com>
2023-01-14 18:32:37 -0800

    refactored env var connect to db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 
 const mongoose = require('mongoose');
 require('dotenv').config({ path: './config/config.env' });
-
+const uri = process.env.MONGO_URI;
 // A function that we can export and use it in our server.js
 const connectDB = async () => {
     
-    const conn = await mongoose.connect(process.env.MONGO_URI, { 
+    const conn = await mongoose.connect(uri, { 
         //  useNewUrlParser: true,
         //  useUnifiedTopology: true,
         //  useFindAndModify: false
